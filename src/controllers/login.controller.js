58a8c7fad0e5dcd3c302e77b16a121f1edcb0db2
Toriqul Islam <toriqulislam296@yahoo.com>
2025-03-07 18:39:19 +0600

    login backend

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,9 +1,10 @@
-const regModel=require('../models/regInfo')
+const RegModel=require('../models/regInfo')
 exports.postLogin=async(req,res)=>{
     try{
      const {emails}=req.body
-     const result=await regModel.findOne({emails})
-     res.send({result})
+     const result=await RegModel.findOne({emails})
+     res.send("ama")
+     console.log("amamm")
     }
     catch(err){
         console.log(err);
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -3,5 +3,5 @@ const router=express.Router()
 const loginController=require('../controllers/login.controller')
 
 
-router.get('/',loginController.getLogin)
+router.get('/',loginController.postLogin)
 module.exports=router
