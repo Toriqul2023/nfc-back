e9abd5cc550d24ca1bc0e0d9b4eedc8add261719
Toriqul Islam <toriqulislam296@yahoo.com>
2025-03-07 18:50:36 +0600

    remove login

--- src/routes/login.route.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router()
-const loginController=require('../controllers/login.controller')
-
-
-router.get('/',loginController.postLogin)
-module.exports=router
